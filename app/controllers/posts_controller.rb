16765678aa86ef91cc5dbe7eb3608deba2f384c9
willienn8254 <willienn@hotmail.com>
2016-02-08 22:05:02 -0500

    Agrega funcionalidad Editar Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,6 +40,32 @@ class PostsController < ApplicationController
 	end
 
 
+	def edit
+
+		@post=Post.find(params[:id])
+
+	end
+
+	def update
+
+		@post=Post.find(params[:id])
+
+		if @post.update(post_params)
+
+			redirect_to posts_path, notice: "El Post fue actualizado con exito"
+
+
+		else
+
+			render :edit
+
+		end
+
+
+
+	end
+
+
 
 	private
 
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,8 @@
 module PostsHelper
+
+	def form_title
+
+		@post.new_record? ? "Publicar Post" : "Editar Post"
+
+	end
 end
