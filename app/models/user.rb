5778c42d578a01cf7f55a7267bd9cc5cef7c4471
willienn8254 <willienn@hotmail.com>
2016-02-13 21:18:11 -0500

    corrige relacion y muestra creador de post

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-has_many :post
+has_many :posts
 has_many :comments
 
   devise :database_authenticatable, :registerable,
