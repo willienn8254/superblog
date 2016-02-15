4024af7e6fdbf83049dea5bae67856bc0d44ce4c
willienn8254 <willienn@hotmail.com>
2016-02-14 19:48:36 -0500

    validad cantidad de caracteres

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,7 +15,7 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 	has_many :comments
 	validates :title, presence: true
-	validates :body, presence: true
+	validates :body, presence: true, length: {minimum: 250}
 
 	
 end
