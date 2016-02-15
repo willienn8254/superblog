a6a92945363a3fe2dc63a04e8cbec8a173b80771
willienn8254 <willienn@hotmail.com>
2016-02-14 20:03:45 -0500

    Agrega comentarios a Post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,4 +11,19 @@
 #
 
 class CommentsController < ApplicationController
+
+
+def create
+  post = Post.find(params[:post_id])
+  post.comments.create(comments_params)
+
+  redirect_to post
+end
+
+private
+  def comments_params
+    params.require(:comment).permit(:body).merge(user: current_user)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
 
 
 
- devise_for :users
+devise_for :users
 root 'posts#index'
-resources :posts
+resources :posts do
+
+	 resources :comments, only: [:create]
+end
 
 
 
